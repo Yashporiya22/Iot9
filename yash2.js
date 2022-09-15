b7749c94bf0cc4765ae2e9a84b32e764120423f0
Yashporiya22 <113594962+Yashporiya22@users.noreply.github.com>
2022-09-15 15:29:56 +0530

    Add files via upload

---
+++ yash2.js
@@ -0,0 +1,30 @@
+function myFunction() {
+ document.getElementById("demoh").style.color = "darkgreen";
+ document.getElementById("demoh").style.backgroundColor = "lightblue";
+ document.getElementById("demoh1").style.color = "darkgreen";
+ document.getElementById("demoh2").style.color = "darkgreen";
+ document.getElementById("demoh3").style.color = "darkgreen";
+
+
+ document.getElementById("demo").style.color = "darkgreen";
+ document.getElementById("demo").style.backgroundColor = "lightblue";
+ document.getElementById("demo1").style.color = "darkgreen";
+ document.getElementById("demo1").style.backgroundColor = "lightblue";
+document.getElementById("but1").style.color = "darkblue";
+ document.getElementById("but1").style.backgroundColor = "lightgreen";
+}
+function myFunction1() {
+ document.getElementById("demoh").style.color = "darkblue";
+ document.getElementById("demoh").style.backgroundColor = "lightgreen";
+ document.getElementById("demoh1").style.color = "darkblue";
+ document.getElementById("demoh2").style.color = "darkblue";
+ document.getElementById("demoh3").style.color = "darkblue";
+
+ document.getElementById("demo").style.color = "darkblue";
+ document.getElementById("demo").style.backgroundColor = "white";
+ document.getElementById("demo1").style.color = "darkblue";
+ document.getElementById("demo1").style.backgroundColor = "white";
+document.getElementById("but1").style.color = "darkgreen";
+ document.getElementById("but1").style.backgroundColor = "lightblue";
+}
+
